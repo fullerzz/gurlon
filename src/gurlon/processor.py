30e94bf213a41958b55a30562dd8c2779f45b69e
Zach Fuller <zach.fuller1222@gmail.com>
2024-11-27 12:58:19 -0700

    added debug logs to DataExporter

--- src/gurlon/processor.py
+++ src/gurlon/processor.py
@@ -4,14 +4,18 @@ from pathlib import Path
 from typing import Any
 
 import orjson
+import structlog
 from dynamodb_json import json_util
 
 from gurlon.dynamodb import DynamoTable
 from gurlon.s3 import DynamoExport, S3Bucket
 
+log: structlog.stdlib.BoundLogger = structlog.get_logger()
+
 
 class DataExporter:
     def __init__(self, aws_region: str, table_name: str, bucket_name: str, key_prefix: str = "gurlon") -> None:
+        log.debug("Initializing DataExporter", aws_region=aws_region, table_name=table_name, bucket_name=bucket_name)
         self.aws_region = aws_region
         self.table: DynamoTable = DynamoTable(table_name, aws_region)
         self.bucket: S3Bucket = S3Bucket(bucket_name, aws_region)
@@ -21,6 +25,7 @@ class DataExporter:
         self.decompressed_files: list[Path] = []
 
     def export_data(self) -> str:
+        log.debug("Exporting data to S3", table_name=self.table.table_name, bucket_name=self.bucket.bucket_name)
         # Export DynamoDB table data to S3
         self.table_export_arn = self.table.export_to_s3(self.bucket.bucket_name, self.key_prefix)
         return self.table_export_arn
@@ -28,10 +33,15 @@ class DataExporter:
     def download_data(self) -> Path:
         if not self.table_export_arn:
             raise ValueError("No export ARN found. Run export_data first")
-
         # Download data from S3
         download_dir = Path.home() / "Downloads" / "dynamodb_exports"
         download_dir.mkdir(exist_ok=True)
+        log.debug(
+            "Downloading data from S3",
+            table_name=self.table.table_name,
+            bucket_name=self.bucket.bucket_name,
+            local_dir=download_dir,
+        )
         self.export_metadata = self.bucket.download_export(download_dir, self.table_export_arn, self.key_prefix)
         # Uncompress the downloaded files
         self.decompress_data()
@@ -39,13 +49,16 @@ class DataExporter:
         combined_path = self.combine_data()
         # Optional: Validate the data
         # Save as CSV or other format
+        log.info("Data downloaded, decompressed, and combined into a single file", combined_path=combined_path)
         return combined_path
 
     def decompress_data(self) -> None:
         if not self.export_metadata:
             raise ValueError("No export metadata found. Run download_data first")
+        log.debug("Decompressing downloaded data", local_dir=self.export_metadata.local_data_dir)
         # Uncompress the downloaded files
         for data_file in self.export_metadata.local_data_files:
+            log.debug("Decompressing file", file=data_file)
             with gzip.open(data_file.as_posix(), "rb") as f:
                 content = f.read()
             decompressed_file = Path(data_file.as_posix().replace(".gz", ""))
@@ -55,6 +68,7 @@ class DataExporter:
 
     def _read_raw_data(self) -> Generator[str, Any, None]:
         for file in self.decompressed_files:
+            log.debug("Reading raw data from file", file=file)
             with file.open("r") as f:
                 lines = f.readlines()
             yield from lines
@@ -73,7 +87,9 @@ class DataExporter:
 
         if self.export_metadata is None:
             raise ValueError("No local export metadata found")
+
         combined_data_path = self.export_metadata.local_data_dir / "combined_data.json"
+        log.debug("Writing combined data to file", file=combined_data_path)
 
         with combined_data_path.open("wb") as f:
             f.write(orjson.dumps(combined_data, option=orjson.OPT_APPEND_NEWLINE))
